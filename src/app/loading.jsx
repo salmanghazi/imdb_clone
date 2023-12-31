6d6e627f79198ce9a8ad27c72d9f6aa633bd008e
msalmanKT <muhammad.salman@gomotive.com>
2023-12-31 19:01:22 +0500

    Added card component

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
 export default function Loading() {
   return (
     <div className="flex justify-center">
-      <img className="h-96" src="spinner.svg" alt="loading..." />
+      <Image
+        className="h-96" src="spinner.svg" alt="loading..."
+        width={500}
+        height={300}
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+        }}
+        >
+      </Image>
     </div>
   );
 }
